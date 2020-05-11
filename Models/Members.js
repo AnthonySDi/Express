4c4d7b1197cc333a98e608443bcdf8c107727a6c
Anthony <stenbergdigeronimo@gmail.com>
2020-05-11 11:31:14 -0700

    Wrote custome Middleware
    Custom middleware to console.log URL that hit the server

---
+++ Models/Members.js
@@ -0,0 +1,26 @@
+/**
+ * Name of this file follows upperCamelCase, since it's the "model"
+ */
+
+const MEMBERS = [
+  {
+    id: 1,
+    name: "John Smith",
+    email: "this.john@example.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "David Frost",
+    email: "this.david@example.com",
+    status: "active",
+  },
+  {
+    id: 1,
+    name: "Kenya Snow",
+    email: "this.kenya@example.com",
+    status: "active",
+  },
+];
+
+module.exports = MEMBERS;
--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 const express = require("express");
-
+const PATH = require("path");
+const MEMBERS = require("./Models/Members");
 const APP = express();
 
-APP.get("/", (req, res) => {});
+/**
+ * @purpose to create customer middleware
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next moves on to the next middleware item.
+ */
+const LOGGER = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
+  next();
+};
+
+// initializes the middleware
+APP.use(LOGGER);
+
+/**
+ * This would require having a route for every page.
+ */
+/*
+APP.get("/", (req, res) => {
+  res.sendFile(PATH.join(__dirname, "public", "index.html"));
+});
+*/
+
+/**
+ * @purpose gets all members
+ */
+APP.get("/api/members", (req, res) => {
+  // .json will return json so I can hit this route on postman
+  //    and see the object MEMBERS
+  res.json(MEMBERS);
+});
+
+// Set static Folder
+/**
+ * @purpose if using static files you can create dynamic routing list this
+ */
+APP.use(express.static(PATH.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 APP.listen(PORT, () => console.log(`Server started on port ${PORT}`));
